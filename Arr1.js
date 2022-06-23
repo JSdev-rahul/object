f37043c68d93df4e2bd83cfb147ec6c8b5f6563c
itsofficial.rahul <98083485+itsofficial-rahul@users.noreply.github.com>
2022-06-23 04:28:30 -0700

    Add files via upload

---
+++ Arr1.js
@@ -0,0 +1,35 @@
+// function allData(a, b) {
+//   const data = ["rahul", "prince", "alok"];
+//   data.splice(b, 1, a);
+//   return data;
+// }
+// console.log(allData("dipesh", 1));
+
+//////////////////////////////////////////////////////////////////
+
+//   find occurance of string
+
+// let str = "hello";
+// let arr = str.split("");
+//
+// let result = arr.reduce(
+//   (prev, curr) => ((prev[curr] = ++prev[curr] || 1), prev),
+//   {}
+// );
+//
+// console.log(result);
+//
+
+/////////////////////////////////////////////////////////////////
+
+// find occurance of array Element
+// const data = [1, 2, 1, 3, 45, 41];
+
+// const result = data.reduce(
+//   (prev, curr) => ((prev[curr] = ++prev[curr] || 1), prev),
+//   {}
+// );
+
+// console.log(result);
+
+//////////////////////////////////////////////////////////////////
